a561ca27130f40c120beb272addc01bc058f7e52
kandukurivishnusri <kandukurivishnusri@gmial.com>
2026-02-25 14:45:02 +0530

    UC2: Hardcoded palindrome logic implemented

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -3,12 +3,29 @@ public class PalindromeChecker {
         System.out.println("--- Palindrome Checker App ---");
         displayWelcomeMessage();
             }
-
+    //Usecase 1
             public static void displayWelcomeMessage() {
                 System.out.println("------------------------------------------");
                 System.out.println("Welcome to the Palindrome Checker App");
                 System.out.println("Version: 1.0.0");
                 System.out.println("------------------------------------------");
+
+//Usecase 2
+                String original = "madam";
+                String reversed = "";
+
+                for (int i = original.length() - 1; i >= 0; i--) {
+                    reversed += original.charAt(i);
+                }
+
+                if (original.equals(reversed)) {
+                    System.out.println(original + " is a palindrome.");
+                } else {
+                    System.out.println(original + " is not a palindrome.");
+                }
+
+//Usecase 3
+
 
     }
 }
