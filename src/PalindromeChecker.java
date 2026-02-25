0464d1b7acf87f6e5ea8b358e2457d3aa30b610b
kandukurivishnusri <kandukurivishnusri@gmial.com>
2026-02-25 15:01:18 +0530

    UC3: Implemented Scanner for user input

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,32 +1,48 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-        System.out.println("--- Palindrome Checker App ---");
         displayWelcomeMessage();
-            }
-    //Usecase 1
-            public static void displayWelcomeMessage() {
-                System.out.println("------------------------------------------");
-                System.out.println("Welcome to the Palindrome Checker App");
-                System.out.println("Version: 1.0.0");
-                System.out.println("------------------------------------------");
+        checkHardcodedPalindrome();
+        checkWithManualReversal();
+    }
+    //UC1
 
-//Usecase 2
-                String original = "madam";
-                String reversed = "";
+    public static void displayWelcomeMessage() {
+        System.out.println("------------------------------------------");
+        System.out.println("Welcome to the Palindrome Checker App");
+        System.out.println("Version: 1.0.0");
+        System.out.println("------------------------------------------");
+    }
 
-                for (int i = original.length() - 1; i >= 0; i--) {
-                    reversed += original.charAt(i);
-                }
+    //UC2
+    public static void checkHardcodedPalindrome() {
+        String original = "madam";
+        String reversed = "";
 
-                if (original.equals(reversed)) {
-                    System.out.println(original + " is a palindrome.");
-                } else {
-                    System.out.println(original + " is not a palindrome.");
-                }
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
 
-//Usecase 3
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+    }
 
+    //UC3
+    public static void checkWithManualReversal() {
+        String original = "radar";
+        String reversed = "";
 
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+        //UC4
     }
 }
-
