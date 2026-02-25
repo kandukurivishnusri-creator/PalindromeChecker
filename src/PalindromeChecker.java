daaffff9084d616d91ab536444d686204c01880c
kandukurivishnusri <kandukurivishnusri@gmial.com>
2026-02-25 14:34:10 +0530

    UC1: Add welcome message module

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,5 +1,15 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
         System.out.println("--- Palindrome Checker App ---");
+        displayWelcomeMessage();
+            }
+
+            public static void displayWelcomeMessage() {
+                System.out.println("------------------------------------------");
+                System.out.println("Welcome to the Palindrome Checker App");
+                System.out.println("Version: 1.0.0");
+                System.out.println("------------------------------------------");
+
     }
 }
+
